6853f1bf42979cc56416e966b4edaf670e2de05b
Developerpritam <pritambag426@gmail.com>
2020-03-06 21:29:41 +0530

    photographer_website
    this is photographer_website..

---
+++ js/javascript.js
@@ -0,0 +1,20 @@
+// sticky navbar animation
+
+window.addEventListener("scroll", function(){
+let menuarea = document.getElementById('menuarea');
+
+if(window.pageYOffset > 0){
+menuarea.classList.add("cus-nav");
+
+}
+else{
+menuarea.classList.remove("cus-nav")
+}
+});
+
+
+
+
+
+
+
